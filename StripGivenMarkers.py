3fb9398c92098bc27929bb364e425f9ece4328fc
m1zaru <jreed1@mail.sfsu.edu>
2016-08-24 16:54:06 -0700

    Readme added

--- StripGivenMarkers.py
+++ StripGivenMarkers.py
@@ -3,3 +3,5 @@ def solution(string,markers):
     for x in markers: 
          string =  re.sub('[ \t]*[\{0}].*[^\n]*?'.format(x),r'',string)
     return string
+
+print solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
